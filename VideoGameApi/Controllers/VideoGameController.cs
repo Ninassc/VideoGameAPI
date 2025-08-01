33f7d7a8f9bd868fdff75d6ddade6b943b60b694
Nina <ninasepulvedacarvalho@gmail.com>
2025-07-31 22:09:09 -0300

    Primeiro Commit

---
+++ VideoGameApi/Controllers/VideoGameController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoGameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+        static private List<VideoGame> videoGames = new()
+        {
+            new VideoGame
+            {
+                Id = 1,
+                Title = "Spider-Man 2",
+                Platform = "PS5",
+                Developer = "Insomniac Games",
+                Publisher = "Sony Interactive Entertainment"
+            },
+
+            new VideoGame
+            {
+                Id = 2,
+                Title = "Fortnite",
+                Platform = "Computer",
+                Developer = "Epic Games",
+                Publisher = "Epic Games"
+            },
+
+            new VideoGame
+            {
+                Id = 3,
+                Title = "Cyberpunk 2077",
+                Platform = "PC",
+                Developer = "CD Projekt Red",
+                Publisher = "CD Projekt"
+            }
+        };
+
+        [HttpGet("List")]
+        public ActionResult<List<VideoGame>> GetVideoGames()
+        {
+            return Ok(videoGames);
+        }
+
+        [HttpGet("ListId/{id}")]
+        public ActionResult<VideoGame> GetVideoGameById(int id)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+
+            if (game is null)
+            {
+                return NotFound();
+            }
+            return Ok(game);
+        }
+
+        [HttpPost("Includ")]
+
+        public ActionResult<VideoGame> IncludVideoGame( VideoGame myGame)
+        {
+            var verifyGame = videoGames.Any(a => a.Title == myGame.Title);
+
+            if (verifyGame)
+            {
+                return BadRequest("Já existe uma tarefa com esse nome");
+            }
+
+            myGame.Id = videoGames.Max(m => m.Id) + 1;
+            videoGames.Add(myGame);
+            return CreatedAtAction(nameof(GetVideoGameById), new {id = myGame.Id}, myGame);
+        }
+
+        [HttpPut("Modify/{id}")]
+        public IActionResult ModifyVideoGame(int id, VideoGame updateGame)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+
+            if (game is null)
+            {
+                return NotFound();
+            }
+
+            game.Title = updateGame.Title;
+            game.Platform = updateGame.Platform;
+            game.Developer = updateGame.Developer;
+            game.Publisher = updateGame.Publisher;
+
+            return NoContent();
+        }
+
+        [HttpDelete("Delete{id}")]
+        public IActionResult DeleteVideoGme(int id)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+
+            if (game is null)
+            {
+                return NotFound();
+            }
+
+            videoGames.Remove(game);
+            return NoContent();
+        }
+
+    }
+}
---
+++ VideoGameApi/Data/VideoGameDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VideoGameApi.Data
+{
+    public class VideoGameDbContext(DbContextOptions<VideoGameDbContext> options) : DbContext(options)
+    {
+        public DbSet<VideoGame> VideoGames => Set<VideoGame>();
+
+
+    }
+}
